7d82e02da8742c7c670d259c7b50bb717cccf516
Dev-Shai <shai.dev.code@gmail.com>
2023-03-19 16:41:29 +0000

    adding notes

--- W13-React mod/03-react-router-lesson/activities/04-Stu-AJAX/solved/src/components/OmdbContainer.js
+++ W13-React mod/03-react-router-lesson/activities/04-Stu-AJAX/solved/src/components/OmdbContainer.js
@@ -13,10 +13,29 @@ class OmdbContainer extends Component {
     search: ""
   };
 
+  // When this component mounts, search for the movie "The Matrix"
+  componentDidMount() {
+    this.searchMovies("The Matrix");
+  }
+
   searchMovies = query => {
     API.search(query)
       .then(res => this.setState({ result: res.data }))
       .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  // When the form is submitted, search the OMDB API for the value of `this.state.search`
+  handleFormSubmit = event => {
+    event.preventDefault();
+    this.searchMovies(this.state.search);
   };
 
   render() {
@@ -27,13 +46,17 @@ class OmdbContainer extends Component {
             <Card
               heading={this.state.result.Title || "Search for a Movie to Begin"}
             >
-              <MovieDetail
-                title={this.state.result.Title}
-                src={this.state.result.Poster}
-                director={this.state.result.Director}
-                genre={this.state.result.Genre}
-                released={this.state.result.Released}
-              />
+              {this.state.result.Title ? (
+                <MovieDetail
+                  title={this.state.result.Title}
+                  src={this.state.result.Poster}
+                  director={this.state.result.Director}
+                  genre={this.state.result.Genre}
+                  released={this.state.result.Released}
+                />
+              ) : (
+                <h3>No Results to Display</h3>
+              )}
             </Card>
           </Col>
           <Col size="md-4">
